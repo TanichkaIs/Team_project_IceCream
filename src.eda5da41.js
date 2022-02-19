a40f51c0c5c2935877f1cad68f8473d0943e6b45
Alsidryid <94798542+Alsidryid@users.noreply.github.com>
2022-02-19 08:55:09 +0000

    Deploying to gh-pages from @ Alsidryid/-team_project_1@33cf978e96149deec5b24f9de28be1b24a447541 🚀

--- src.eda5da41.js
+++ src.eda5da41.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/-team_project_1/src.32bd8d14.js.map
+//# sourceMappingURL=/-team_project_1/src.eda5da41.js.map
